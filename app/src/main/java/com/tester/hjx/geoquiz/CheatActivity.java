7fe7914cdc5620b40cca57bad654077496b443f9
hujunxiang <hujunxiang@panda.tv>
2018-07-19 15:51:09 +0800

    add api21 method

--- app/src/main/java/com/tester/hjx/geoquiz/CheatActivity.java
+++ app/src/main/java/com/tester/hjx/geoquiz/CheatActivity.java
@@ -1,10 +1,13 @@
 package com.tester.hjx.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -42,6 +45,28 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShowButton(true);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth()/2;
+                    int cy = mShowAnswerButton.getHeight()/2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = null;
+                    anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton,cx,cy,radius,0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                }
+
+                else {
+                    mShowAnswerButton.setVisibility(View.VISIBLE);
+                }
+
+
             }
         });
 
