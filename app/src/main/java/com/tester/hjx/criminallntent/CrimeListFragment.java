0ee9688fbd2c35f74fbcb54215799fa0eceb8a1b
hjx19 <hjx19881022@126.com>
2018-07-23 23:49:50 +0800

    add click op

--- app/src/main/java/com/tester/hjx/criminallntent/CrimeListFragment.java
+++ app/src/main/java/com/tester/hjx/criminallntent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -25,21 +26,27 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private Crime mCrime;
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime,parent,false));
+            itemView.setOnClickListener(this);
             mTitleTextView = (TextView)itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView)itemView.findViewById(R.id.crime_date);
+        }
+
+        @Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(),mCrime.getTitle()+" clicked!", Toast.LENGTH_SHORT).show();
         }
 
         public void bind(Crime crime){
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
-            mTitleTextView.setText(mCrime.getDate().toString());
+            mDateTextView.setText(mCrime.getDate().toString());
         }
     }
 
