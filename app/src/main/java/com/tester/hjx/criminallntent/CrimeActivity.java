d4e3e3f01e67e06e07e97d1189623ce779501e19
hujunxiang <hujunxiang@panda.tv>
2018-07-27 19:20:54 +0800

    fragment argument

--- app/src/main/java/com/tester/hjx/criminallntent/CrimeActivity.java
+++ app/src/main/java/com/tester/hjx/criminallntent/CrimeActivity.java
@@ -28,7 +28,7 @@ import java.util.UUID;
 //    }
 //}
 public class CrimeActivity extends SingleFragmentActivity {
-    public static  final  String EXTRA_CRIME_ID = "com.tester.hjx.criminallntent,crime_id";
+    private static  final  String EXTRA_CRIME_ID = "com.tester.hjx.criminallntent,crime_id";
     public static Intent newIntent(Context packageContext, UUID crimeId){
         Intent intent = new Intent(packageContext,CrimeActivity.class);
         intent.putExtra(EXTRA_CRIME_ID,crimeId);
