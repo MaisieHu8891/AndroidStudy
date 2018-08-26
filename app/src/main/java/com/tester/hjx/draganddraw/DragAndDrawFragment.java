750ac20362d9fa029db214d2646d9cc46934f30e
hjx19 <hjx19881022@126.com>
2018-08-26 21:56:18 +0800

    init

---
+++ app/src/main/java/com/tester/hjx/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,21 @@
+package com.tester.hjx.draganddraw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DragAndDrawFragment extends Fragment {
+
+    public static DragAndDrawFragment newInstance(){
+        return new DragAndDrawFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup contanier, Bundle saveInstanceState){
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, contanier, false);
+        return v;
+    }
+
+}
