97e2d8d3427e26276d56e8413f76104f33159906
hjx19 <hjx19881022@126.com>
2018-09-04 22:36:23 +0800

    add order broadcast intent

--- app/src/main/java/com/tester/hjx/photogallery/VisibleFragment.java
+++ app/src/main/java/com/tester/hjx/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package com.tester.hjx.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.widget.Toast;
 
 public class VisibleFragment extends Fragment {
@@ -12,7 +14,9 @@ public class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),"Got a broacast: "+intent.getAction(),Toast.LENGTH_LONG).show();
+//            Toast.makeText(getActivity(),"Got a broacast: "+intent.getAction(),Toast.LENGTH_LONG).show();
+            Log.i(TAG, "canceling notificaton");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 
